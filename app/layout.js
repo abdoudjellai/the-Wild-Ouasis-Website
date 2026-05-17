0b80acd3384f05b8c2ce47172999106321c1c9be
abdoudjellai <abdoudjellali65@gmail.com>
2026-05-17 14:44:08 +0100

    adding metadata

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,16 @@
 import Navigation from "./_components/Navigation";
 import Logo from "./_components/Logo";
 import "@/app/_styles/globals.css";
+
+export const metadata = {
+  // title: "The Wild Oasis",
+   title : {
+    template : "%s | The Wild Oasis",
+    default : "The Wild Oasis" 
+   } , 
+  description: "Luxurious cabin hotel , located in the heart of Italian  Dolomites , surrounded by beatiful mountains and dark forsts",
+  
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
